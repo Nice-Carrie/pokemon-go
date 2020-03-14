b463845e48b97db997d74b580ef5c85b69254f9a
cherrie <cherrien@hotmail.com>
2020-03-14 23:31:38 +0800

    Initial Commit

---
+++ pokemon go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemon go
+//
+//  Created by Carrie Lin on 2020/3/14.
+//  Copyright © 2020 Carrie Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
